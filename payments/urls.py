d81206becd31e728579c561d413caabf838762fe
mrabedini <abed@shipup.net>
2020-03-17 23:58:54 +0330

    [payments]: * Add charge view. * Close the payment cycle. * Send charge success back to stripe.

--- payments/urls.py
+++ payments/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.HomePageView.as_view(), name="home")]
+urlpatterns = [
+    path("", views.HomePageView.as_view(), name="home"),
+    path("charge/", views.charge, name="charge"),
+]
 
